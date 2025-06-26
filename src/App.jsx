5535f98e41643615521b9faaa93d98f76c3ca284
ManuelCrispino <manuel.barman96@gmail.com>
2025-06-26 11:10:43 +0200

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,15 @@ function App() {
    })
   }
 
-  function handleDeleteTask(){
-
+  function handleDeleteTask(taskId){
+   setProjectsState((prevState)=>{
+    
+    return{
+      ...prevState,
+      tasks: prevState.tasks.filter(task => task.id !== taskId)
+    }
+   })
+    console.log(projectsState);
   }
 
   function handleDeleteSelectedProject(){
--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+import Modal from "./Modal";
 
-export default function NewTask({onAdd,onDelete}){
+export default function NewTask({onAdd}){
+     const modal = useRef()
 
     const [enteredTask,setEnteredTask]= useState('');
 
@@ -9,14 +11,32 @@ export default function NewTask({onAdd,onDelete}){
     };
 
      function handleOnClick(){
+        if(enteredTask.trim() === ''){
+          return modal.current.open();
+        }
         onAdd(enteredTask);
         setEnteredTask('');
     };
 
-    return(
+    return(<>
+            <Modal ref={modal} buttonCaption={"Close"}>
+            <h2 className='text-xl font-bold txt-stone-700 my-4'>Invalid Input</h2>
+            <p className='text-stone-600 mb-4 '>Oops ... looks like you forgot to enter a value .</p>
+            <p className='text-stone-600 mb-4 '>Please make sure you provide a valid value for every input field. </p>
+        </Modal>
         <div>
-            <input type="text" value={enteredTask} onChange={handleAddTask} className="w-64 px-2 py-1 rounded-sm bg-stone-200 "/>
-            <button onClick={handleOnClick} className="text-stone-700 hover:text-stone-950 px-4">Add Task</button>
+            <input 
+            type="text"
+            value={enteredTask} 
+            onKeyDown={(e) => {
+                 if (e.key === "Enter" ){
+                e.preventDefault(); handleOnClick();
+            }}} 
+            onChange={handleAddTask} 
+            className="w-64 px-2 py-1 rounded-sm bg-stone-200 "
+            />
+            <button onClick={handleOnClick} className="text-stone-600 hover:text-stone-950 px-4">Add Task</button>
         </div>
+        </>
     )
 }
